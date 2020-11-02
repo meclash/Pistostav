fb28e63493b0d0154ddc9bca057f01db8074d6ff
Marek <marekmiklas.92@gmail.com>
2020-11-02 03:41:25 +0100

    still working on the responsive navbar

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('nav.links');
-    
-    burger.addEventListener('click',()=>{
-        nav.classList.toggle('nav-active');
-    
+    const nav = document.querySelector('.navigation');
+    const navLinks = document.querySelectorAll('.nav-link');
+  
+  
+    burger.addEventListener('click', () => {
+      //Toggle Nav
+      nav.classList.toggle('navigation-active');
+  
+      //Animate Links
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = '';
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+          }
+      });
+      //Burger Animation
+      burger.classList.toggle('toggle');
     });
-}
-
-navSlide();
+  }
+  
+  navSlide();
