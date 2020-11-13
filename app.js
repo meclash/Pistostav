8f00d02b50d1ff9e4c57e9d5f14d58ecd1810758
Marek <marekmiklas.92@gmail.com>
2020-11-13 07:34:56 +0100

    hamburger menu working

---
+++ app.js
@@ -0,0 +1,8 @@
+let nav = document.querySelector(".nav");
+let hamburgerMenu = document.querySelector(".hamburger-menu")
+
+hamburgerMenu.addEventListener("click", function () {
+    nav.classList.toggle("menu-open");
+})
+
+
