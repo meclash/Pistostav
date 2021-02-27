2cee4ae8953a9b6c81b97b0dd5908ee644a6635a
Marek <marekmiklas.92@gmail.com>
2021-02-27 03:55:10 +0100

    service card have now images as css background imagas

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("carousel__slide");
-  var dots = document.getElementsByClassName("carousel__indicator");
+  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
@@ -36,48 +36,4 @@ function showSlides(n) {
     }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-
-
-
-
-
-
-
-
-
-
-// // REVIEW CAROUSEL
-
-// const track = document.querySelector(".carousel__track");
-// const slides = Array.from(track.children);
-// const nextButton = document.querySelector(".carousel__button--right");
-// const prevButton = document.querySelector(".carousel__button--left");
-// const dotsNav = document.querySelector(".carousel__nav");
-// const dots = Array.from (dotsNav.children);
-
-// const slideWidth = slides[0].getBoundingClientRect().width;
-
-// //arrange the slide next to one another
-// const setSlidePosition = (slide, index) => {
-//     slides.style.left = slideWidth * index + "px";
-// };
-
-// slides.forEach(setSlidePosition);
-
-// // moving slides to the left
-
-// //when I click right ,nove slides to the right
-
-// nextButton.addEventListener ("click", e => {
-//     const currentSlide = track.querySelector(".currentSlide");
-//     const nextSlide = currentSlide.nextElementSibling;
-//     const amountToMove = nextSlide.style.left;
-//     track.style.transform = "translateX(-" + amountToMove
-
-//     //move to the next slide
-// })
-
-
-// //moving slides when clicking on carousel nav
-
-
+}
